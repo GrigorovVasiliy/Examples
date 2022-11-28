c9569190e0a86e6d3001444fc862549b3b251a41
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-11-28 14:57:10 +0700

    Random

--- Example003 Sum/Program.cs
+++ Example003 Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 10;
-int numberB = 5;
-int result = numberA + numberB;
+﻿double numberA = new Random().Next(1,10); // учитывать будет цифры от 1 до 10
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+double result = numberA + numberB;
 Console.WriteLine(result);
