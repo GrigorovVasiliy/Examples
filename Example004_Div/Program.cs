cbc2015e27eead9d32125d97428a4dc0c6f43e38
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-03 10:41:23 +0700

    metodArray

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,4 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1,10); // учитывать будет цифры от 1 до 10
+double numberB = new Random().Next(1,10);
 double result = numberA / numberB;
 Console.WriteLine(result);
