755f0563e393cc273361a423080f32cdeb137063
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-04 17:14:53 +0700

    Большая цыфра из двузначного числа

---
+++ Seminar02/Work001/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит 
+//случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//78 -> 8 
+//12-> 2 
+//85 -> 8
+
+
+Console.Clear();
+
+int number = new Random(). Next(10, 100);     // (10, 99)
+Console.WriteLine($"Сгенерированное число: {number}");
+int n1 = number / 10;
+int n2 = number % 10;
+    if (n1 > n2)
+        Console.WriteLine(n1);
+    else
+        Console.WriteLine(n2);
+
+
+//int n = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите 2-ое число: ");
+//int m = Convert.ToInt32(Console.ReadLine());
+//if (n % m == 0)
+   // Console.WriteLine("Кратно");
+//else
+    //Console.WriteLine($"Не кратно, остаток {n % m}");
+
