4b0fc2aa6d596c0f687b8d0e7226be7aaa82ae44
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-04 18:04:54 +0700

    Проверка на кратность и нахождение остатко

---
+++ Seminar02/Work002/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+    if(a % b == 0)
+        Console.WriteLine("Кратно");
+    else
+        Console.WriteLine($"Не кратно, остаток {a % b}");
