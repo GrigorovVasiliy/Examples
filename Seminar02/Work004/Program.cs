ebb1db2353326fbc76e3d5135f802bece5398449
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-04 21:18:46 +0700

    Проверка лыух чисел на единый  делитель

---
+++ Seminar02/Work004/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+    if(a * a == b)
+        Console.WriteLine("yes");
+    else
+{
+    if(b * b == a)
+    Console.WriteLine("yes");
+    else 
+    Console.WriteLine("no");
+}       
+
