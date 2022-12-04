82bd939dfcef0fca2dc0cf07e01dbf27e8f28483
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-04 18:17:12 +0700

    Проверка кратности одновременно на 7 и 23

---
+++ Seminar02/Work003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+    if(a % 7 == 0 && a % 23 == 0)
+        Console.WriteLine("Да");
+    else
+        Console.WriteLine("Нет");
